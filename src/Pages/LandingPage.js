ffd4fd81412da38eff6f064de278852dfd8c7afb
Your Name <you@example.com>
2018-11-10 15:11:36 -0600

    finished landingpage

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -9,11 +9,12 @@ export default class LandingPage extends Component {
     <div>
       <div id = "title">
         <div id = "ds">
+        <style>@import url('https://fonts.googleapis.com/css?family=Questrial');</style>
         DIGITAL SOLUTIONS
         </div>
 
         <div id = "sg">
-        <span id="gray">FOR </span> SOCIAL GOOD
+        <span id="gray">FOR</span> <span id = "social">SOCIAL</span> GOOD
         </div>
       </div>
     </div>
