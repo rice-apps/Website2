61c573e0e8e2c437bd16ebc45236929f12f7feb5
Your Name <you@example.com>
2018-11-12 20:04:27 -0600

    added animation

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -5,18 +5,34 @@ export default class LandingPage extends Component {
 
 
   render() {
+
+    var title = document.querySelector('.title');
+
+    var intro = anime({
+      targets: title,
+      opacity: 0.5,
+      easing: 'easeInOutQuad',
+      translateX: 250
+    });
+
     return(
     <div>
       <div id = "title">
-        <div id = "ds">
+        <div id = "ds" class = "initialText">
         <style>@import url('https://fonts.googleapis.com/css?family=Questrial');</style>
         DIGITAL SOLUTIONS
         </div>
 
-        <div id = "sg">
+        <div id = "sg" class = "initialText">
         <span id="gray">FOR</span> <span id = "social">SOCIAL</span> GOOD
         </div>
+
+        <div id = "riceApps">
+        riceApps
+        </div>
+
       </div>
+
     </div>
   )
 
