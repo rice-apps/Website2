8e328e4d0fa04b2eeeb4e34db81d6af17b50d185
Your Name <you@example.com>
2018-11-17 14:34:00 -0600

    added circles

--- src/Pages/JoinUs.js
+++ src/Pages/JoinUs.js
@@ -8,7 +8,7 @@ export default class LandingPage extends Component {
     return(
     <div>
     <style>@import url('https://fonts.googleapis.com/css?family=Questrial');</style>
-    <div id = "content">
+    <div id = "content-JoinUs">
     <div id = "ContactUsTitle">
     Give us a shot.
     </div>
