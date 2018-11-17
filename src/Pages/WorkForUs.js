496960e04f73deb4bb416c76f498bb07548c5fdb
Your Name <you@example.com>
2018-11-17 13:38:39 -0600

    created joinus page

--- src/Pages/WorkForUs.js
+++ src/Pages/WorkForUs.js
@@ -7,6 +7,17 @@ export default class LandingPage extends Component {
   render() {
     return(
     <div>
+
+    <style>@import url('https://fonts.googleapis.com/css?family=Questrial');</style>
+    <div id = "content-WorkForUs">
+    <div id = "JoinUsText">
+    WE LOVE NEW PEOPLE.
+    </div>
+    <div id = "JoinUsTitle">
+    Help us impact more.
+    </div>
+    </div>
+
     </div>
   )
 
