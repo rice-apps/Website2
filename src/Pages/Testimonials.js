61f279d77fa8c667f792b3972631e9f8a34e30ee
Pranay Mittal <pranaym12@gmail.com>
2018-11-17 16:02:04 -0600

    shryans and rice

--- src/Pages/Testimonials.js
+++ src/Pages/Testimonials.js
@@ -8,7 +8,13 @@ export default class LandingPage extends Component {
     return(
       <div className= "TestmonialsWrapper">
         <div className = "quote">
-          "Riceapps was instrumental to my development not only as a developer, but as a leader -- I was able to communicate with clients clearly and thoughtfully, while working efficiently with my team. I emerged with a better understanding of the principles used software and product management."
+          Riceapps was instrumental to my development not only as a developer, but as a leader — I was able to communicate with clients clearly and thoughtfully, while working efficiently with my team. I emerged with a better understanding of the principles used software and product management.
+        </div>
+        <div id = "shryans">
+         — Shryans Goyal
+        </div>
+        <div id="teamLead">
+          Team Lead, Rice U. '21
         </div>
       </div> 
   )
