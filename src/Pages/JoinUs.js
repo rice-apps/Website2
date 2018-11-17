81378eda439bfb5c809226c7e5a2e5117f733b92
Victor Song <vsong1618@gmail.com>
2018-11-17 15:22:04 -0600

    Changed all the fonts to HK Grotesk

--- src/Pages/JoinUs.js
+++ src/Pages/JoinUs.js
@@ -7,7 +7,7 @@ export default class LandingPage extends Component {
   render() {
     return(
     <div>
-    <style>@import url('https://fonts.googleapis.com/css?family=Questrial');</style>
+    {/* <style>@import url('https://fonts.googleapis.com/css?family=Questrial');</style> */}
     <div id = "content-JoinUs">
     <div id = "ContactUsTitle">
     Give us a shot.
