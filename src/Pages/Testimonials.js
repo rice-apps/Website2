a27f953c04742542a8a9cad8c81f73292325134d
Victor Song <vsong1618@gmail.com>
2018-11-19 20:07:51 -0600

    Testimonials page looks better now

--- src/Pages/Testimonials.js
+++ src/Pages/Testimonials.js
@@ -5,19 +5,18 @@ export default class LandingPage extends Component {
 
 
   render() {
-    return(
-      <div className= "TestmonialsWrapper">
-        <div className = "TestimonialParagraph">
+    return (
+      <div className="TestmonialsWrapper">
+        <div id="test-circle"></div>
+        <div className="TestimonialParagraph">
           "Riceapps was instrumental to my development not only as a developer, but as a <em> leader </em> — I was able to communicate with clients clearly and thoughtfully, while working <em> efficiently </em> with my team. I emerged with a better understanding of the principles used software and product management."
         </div>
-        <div id = "shryans">
-         — Shryans Goyal
-        </div>
-        <div id="teamLead">
-          Team Lead, Rice U. '21
+        <div className="nameWrapper">
+          <span id="shryans">— Shryans Goyal</span>
+          <span id="teamLead">Team Lead, Rice U. '21</span>
         </div>
       </div>
-  )
+    )
 
   }
 
