8b5e2904c9fb35fdff0c1ad0466d3d89a51dbf51
Pranay Mittal <pranaym12@gmail.com>
2018-11-22 14:48:58 -0600

    fix indents

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     //if it's closed, open it
     this.setState({ clicked: !this.state.clicked })
   }
-   closeSidebar() {
+  closeSidebar() {
     //Call this function whenever user clicks outside of the sidebar menu
     if (this.state.clicked) {
       //But only sets clicked to False when clicked is True
@@ -36,44 +36,44 @@ class App extends Component {
     //start sidebar-menu
     let menuStatus = this.state.clicked ? "open" : "";
     let button_classes = this.state.clicked
-    ? "hamburger hamburger--collapse is-active" : "hamburger hamburger--collapse ";
+      ? "hamburger hamburger--collapse is-active" : "hamburger hamburger--collapse ";
     //end of sidebar-menu
     return (
       <div>
-            <button onClick={() => this.handleClick()}
-              id="hamburger" class={button_classes} type="button">
-              <span class="hamburger-box">
-                <span class="hamburger-inner"></span>
-              </span>
-            </button>
-             {/*<div id="sidebar" class={sidebar_menu_classes}>*/}
+        <button onClick={() => this.handleClick()}
+          id="hamburger" class={button_classes} type="button">
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </span>
+        </button>
+        {/*<div id="sidebar" class={sidebar_menu_classes}>*/}
 
-            <div id = "menu" class = {menuStatus}>
-            <ScrollIntoView selector="#home">
-              <div class = "sidebarText">HOME</div>
-            </ScrollIntoView>
-            <ScrollIntoView selector="#aboutUs">
-              <div class = "sidebarText">MISSION</div>
-            </ScrollIntoView>
-            <ScrollIntoView selector="#projects">
-              <div class = "sidebarText">PROJECTS</div>
-            </ScrollIntoView>
-            <ScrollIntoView selector="#testimonials">
-              <div class = "sidebarText">TESTIMONIALS</div>
-            </ScrollIntoView>
-            <ScrollIntoView selector="#contactUs">
-              <div class = "sidebarText">CONTACT US</div>
-            </ScrollIntoView>
-            </div>
+        <div id="menu" class={menuStatus}>
+          <ScrollIntoView selector="#home">
+            <div class="sidebarText">HOME</div>
+          </ScrollIntoView>
+          <ScrollIntoView selector="#aboutUs">
+            <div class="sidebarText">MISSION</div>
+          </ScrollIntoView>
+          <ScrollIntoView selector="#projects">
+            <div class="sidebarText">PROJECTS</div>
+          </ScrollIntoView>
+          <ScrollIntoView selector="#testimonials">
+            <div class="sidebarText">TESTIMONIALS</div>
+          </ScrollIntoView>
+          <ScrollIntoView selector="#contactUs">
+            <div class="sidebarText">CONTACT US</div>
+          </ScrollIntoView>
+        </div>
         <div onClick={() => this.closeSidebar()}>
           <div id="home">
             <LandingPage />
           </div>
-          <div id="aboutUs"><AboutUs/></div>
-          <div id="projects"><Projects/></div>
-          <div id="testimonials"><Testimonials/></div>
-          <div id="contactUs"><JoinUs/></div>
-          <div id="joinUs"><WorkForUs/></div>
+          <div id="aboutUs"><AboutUs /></div>
+          <div id="projects"><Projects /></div>
+          <div id="testimonials"><Testimonials /></div>
+          <div id="contactUs"><JoinUs /></div>
+          <div id="joinUs"><WorkForUs /></div>
           {/*<div id="testimonials"><Testimonials/></div>  */}
         </div>
       </div>
