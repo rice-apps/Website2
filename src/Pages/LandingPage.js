84002cb9d4c1657f4a4de9eab93c8130f7d85c6d
Pranay Mittal <pranaym12@gmail.com>
2019-01-19 12:29:21 -0600

    reformatted code

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -4,25 +4,25 @@ export default class LandingPage extends Component {
   render() {
 
 
-    return(
-    <div>
-      <div id = "title">
-        <div id = "ds" class = "initialText">
-        {/* <style>@import url('https://fonts.googleapis.com/css?family=Questrial');</style> */}
-        DIGITAL SOLUTIONS
+    return (
+      <div>
+        <div id="title">
+          <div id="ds" class="initialText">
+            {/* <style>@import url('https://fonts.googleapis.com/css?family=Questrial');</style> */}
+            DIGITAL SOLUTIONS
         </div>
 
-        <div id = "sg" class = "initialText">
-        <span id="gray">FOR</span> <span id = "social">SOCIAL</span> GOOD
+          <div id="sg" class="initialText">
+            <span id="gray">FOR</span> <span id="social">SOCIAL</span> GOOD
         </div>
 
-        <div id = "riceApps">
-        riceApps
+          <div id="riceApps">
+            riceApps
+        </div>
+
         </div>
 
       </div>
-
-    </div>
-  )
+    )
   }
 }
