ce2ddfc1ade3d57b1138e1a06baf026e6b8c077e
Jiin  Rhew <jiinrhew@jiins-mbp.dyndns.rice.edu>
2019-01-26 15:07:43 -0600

    Projects and About Us circle size reduced, carousel added for Testimonials

--- src/Pages/Partners.js
+++ src/Pages/Partners.js
@@ -4,13 +4,11 @@ import FOTW from '../fotw.jpg';
 import BakerRipley from '../bakerripley.png';
 import CsClub from '../csclub.png';
 import Coffeehouse from '../chaus.jpg';
-import Fade from 'react-reveal/Fade';
 export default class Partners extends Component {
   render() {
 
     return(
     <div id = "trash">
-    <Fade duration = {3000}>
     <div id="text"> Our Partners</div>
     <div id="logos">
     <img id= "image" src= {FOTW} />
@@ -18,7 +16,6 @@ export default class Partners extends Component {
     <img id ="image" src={CsClub} />
     <img id ="image" src={Coffeehouse} />
     </div>
-    </Fade>
     </div>
   )
   }
--- src/Pages/Testimonials.js
+++ src/Pages/Testimonials.js
@@ -1,6 +1,50 @@
 import React, { Component } from 'react';
+import makeCarousel from 'react-reveal/makeCarousel';
+import Slide from 'react-reveal/Slide';
+import styled, { css } from 'styled-components';
 import Fade from 'react-reveal/Fade';
 import './Testimonials.css';
+
+
+const width = '80vw', height='25vw';
+
+const Container = styled.div`\
+  overflow: hidden;
+  width: ${width};
+`;
+const Children = styled.div`
+  width: ${width};
+  position: relative;
+  height: ${height};
+`;
+
+const Dot = styled.span`
+  font-size: 1.5em;
+  cursor: pointer;
+  color: #40BFB4;
+  text-shadow: 1px 1px 1px #fff;
+  user-select: none;
+`;
+const Dots = styled.span`
+  text-align: center;
+  width: ${width};
+  z-index: 100;
+`;
+const CarouselUI = ({ position, total, handleClick, children }) => (
+  <Container>
+    <Children>
+      {children}
+    </Children>
+    <Dots>
+      {Array(...Array(total)).map( (val, index) =>
+        <Dot key={index} onClick={handleClick} data-position={index}>
+          {index === position ? '● ' : '○ ' }
+        </Dot>
+      )}
+    </Dots>
+  </Container>
+);
+const Carousel = makeCarousel(CarouselUI);
 
 export default class LandingPage extends Component {
 
@@ -8,18 +52,28 @@ export default class LandingPage extends Component {
   render() {
     return (
       <div className="TestmonialsWrapper">
-      <Fade bottom duration = {3000}>
-        <div className="TestimonialParagraph">
-          "Riceapps was instrumental to my development not only as a developer, but as a <em> leader </em> — I was able to communicate with clients clearly and thoughtfully, while working <em> efficiently </em> with my team. I emerged with a better understanding of the principles used software and product management."
-        </div>
-      </Fade>
+      <Carousel>
+        <Slide right>
+          <div className="TestimonialParagraph">
+            "Riceapps was instrumental to my development not only as a developer, but as a <em> leader </em> — I was able to communicate with clients clearly and thoughtfully, while working <em> efficiently </em> with my team. I emerged with a better understanding of the principles used software and product management."
+          </div>
+          <div className="nameWrapper">
+            <span id="shryans">— Shryans Goyal</span>
+            <span id="teamLead">Team Lead, Rice U. '21</span>
+          </div>
+        </Slide>
+        <Slide right>
+          <div className="TestimonialParagraph">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet turpis pharetra rhoncus accumsan. Maecenas non neque nibh. Aenean quis nunc ipsum. Nunc fringilla tortor in erat imperdiet aliquet. Vivamus placerat nec odio sit amet pretium. Nullam vestibulum arcu eget lobortis elementum. In imperdiet rutrum odio a lobortis."
+          </div>
+          <div className="nameWrapper">
+            <span id="shryans">— A Website</span>
+            <span id="teamLead">Team Lead, Rice U. '21</span>
+          </div>
+        </Slide>
 
-      <Fade bottom duration = {3000} delay = {700}>
-        <div className="nameWrapper">
-          <span id="shryans">— Shryans Goyal</span>
-          <span id="teamLead">Team Lead, Rice U. '21</span>
-        </div>
-      </Fade>
+      </Carousel>
+
       </div>
     )
 
