eeb91442a65e6feaa05c33fd2109df376dd8fe6d
Pranay Mittal <pranaym12@gmail.com>
2019-01-26 14:06:54 -0600

    initialize fake stuff

---
+++ src/Pages/BeakSpeak.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './AboutUs.css';
+
+export default class AboutUs extends Component {
+    render() {
+        return(
+          <div className="AboutUsWrapper">
+              <div id="AboutUsParagraph">
+                  {/* <style>
+                      @import url('https://fonts.googleapis.com/css?family=Questrial');
+                  </style> */}
+                  <p>
+                      We emphasize the <em>product</em> and the <em>process</em> — our developers learn and apply new technologies to custom tailored products that directly impact client organizations.
+                  </p>
+              </div>
+          </div>
+          )
+
+  }
+
+}
