1d85b0b3f16affd9f9a0f3bb7a3d26ce6f3ce56f
Pranay Mittal <pranaym12@gmail.com>
2019-01-31 18:35:37 -0600

    done a lot

--- src/Pages/BeakSpeak.js
+++ src/Pages/BeakSpeak.js
@@ -5,17 +5,50 @@ export default class BeakSpeak extends Component {
     render() {
         return (
             <div>
-                <div className="BeakSpeakWrapper">
-                    <div id="BeakSpeakParagraph">
-                        {/* <style>
-                        @import url('https://fonts.googleapis.com/css?family=Questrial');
-                    </style> */}
-                        <p>
-                            We emphasize the <em>product</em> and the <em>process</em> — our developers learn and apply new technologies to custom tailored products that directly impact client organizations.
-                    </p>
+                <div className="pageWrapper">
+                    <div id="content">
+                        <div id="projectName">
+                            BeakSpeak.
+                        </div>
+                        <div class="paragraphs" id="projectDescription">
+                            Insert description of project here. Lorem ipsum dolor sit amet, erat postea argumentum id mei, ius quaeque epicurei ut. Doming aperiri fabulas pri id, id dolor percipitur nec, sale error ei per. Dolores
+                        </div>
+                        <div>
+                            <div id="teamAndProjectScope" class="paragraphs">
+                                <div class="teamAndProjectScopeLabel" >Team</div>
+                                <div class="paragraphs">Shryans Goyal, Danny Andreini</div>
+                            </div>
+                            <div class="paragraphs">
+                                <div class="teamAndProjectScopeLabel" id="projectScope">Project Scope</div>
+                                <div class="paragraphs">Design, Machine Learning, User Research, Cloud Compiling, Javascript, Beta Testing</div>
+
+                            </div>
+                        </div>
+
+                        <div>
+                            <div class="caption">OUR PROCESS</div>
+                        </div>
+                        <div>
+                            <div class="caption">THE PROBLEM</div>
+                            <div class="captionDescription">Insert description of project here. Lorem ipsum dolor sit amet, erat postea argumentum id mei, ius quaeque epicurei ut. Doming aperiri fabulas pri id, id dolor percipitur nec, sale error ei per. Dolores insolens mea ex, eam definitiones reprehendunt ei, sed eu commune omnesque sadipscing. Diam admodum offendit usu at, ei aperiam interesset quo.</div>
+                        </div>
+                        <div>
+                            <div class="caption">OUR SOLUTION</div>
+                            <div class="captionDescription">Insert description of project here. Lorem ipsum dolor sit amet, erat postea argumentum id mei, ius quaeque epicurei ut. Doming aperiri fabulas pri id, id dolor percipitur nec, sale error ei per. Dolores insolens mea ex, eam definitiones reprehendunt ei, sed eu commune omnesque sadipscing. Diam admodum offendit usu at, ei aperiam interesset quo.</div>
+                        </div>
+                        
                     </div>
+                    
                 </div>
-                <div className="projectsPageWrapper">
+
+                {/* <div id="BeakSpeakParagraph">
+                            
+                            <p>
+                                We emphasize the <em>product</em> and the <em>process</em> — our developers learn and apply new technologies to custom tailored products that directly impact client organizations.
+                            </p>
+                </div> */}
+
+                {/* <div className="projectsPageWrapper">
                     <div id="beakspeak">
                         <div id="BsTitle"><h1>Beakspeak</h1></div>
                         <div className="circle">
@@ -45,7 +78,7 @@ export default class BeakSpeak extends Component {
                             <div className="projectText">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
 
         )
