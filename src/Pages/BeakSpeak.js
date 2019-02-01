2cf96678217906beee1543223462c2a3f8b26483
Pranay Mittal <pranaym12@gmail.com>
2019-01-31 19:09:51 -0600

    remove timeline.

--- src/Pages/BeakSpeak.js
+++ src/Pages/BeakSpeak.js
@@ -24,10 +24,6 @@ export default class BeakSpeak extends Component {
 
                             </div>
                         </div>
-
-                        <div>
-                            <div class="caption">OUR PROCESS</div>
-                        </div>
                         <div>
                             <div class="caption">THE PROBLEM</div>
                             <div class="captionDescription">Insert description of project here. Lorem ipsum dolor sit amet, erat postea argumentum id mei, ius quaeque epicurei ut. Doming aperiri fabulas pri id, id dolor percipitur nec, sale error ei per. Dolores insolens mea ex, eam definitiones reprehendunt ei, sed eu commune omnesque sadipscing. Diam admodum offendit usu at, ei aperiam interesset quo.</div>
