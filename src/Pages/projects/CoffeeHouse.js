9eb33f1bdc854cb64683328f942715e94cb3b45c
Pranay Mittal <pranaym12@gmail.com>
2019-02-02 15:36:32 -0600

    Merged BeakSpeak projects page into Sharon's code which routes home page into separate projects pages. pretty good tbh

--- src/Pages/projects/CoffeeHouse.js
+++ src/Pages/projects/CoffeeHouse.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
+import './ProjectsTemplate.css';
+// import {BrowserRouter as Router, Route} from 'react-router-dom'
+// import React from "react";
+import App from "../../App.js";
+import {BrowserRouter as Router, Link} from "react-router-dom";
+
 
 export default class CoffeeHouse extends Component {
     render() {
-      return (
-        <div>
-        </div>
-      )
+        return (
+            <Router>
+                <div>             
+                    <Link to="/" onClick= {() => window.location.reload()} >
+                        <img class = "homeIcon" src={require("../../home-icon.png")}/>
+                    </Link>
+                    <div className="pageWrapper">               
+                        <div id="content">
+                            <div id="projectName">
+                                BeakSpeak.
+                            </div>
+                            <div class="paragraphs" id="projectDescription">
+                                Insert description of project here. Lorem ipsum dolor sit amet, erat postea argumentum id mei, ius quaeque epicurei ut. Doming aperiri fabulas pri id, id dolor percipitur nec, sale error ei per. Dolores
+                            </div>
+                            <div>
+                                <div id="teamAndProjectScope" class="paragraphs">
+                                    <div class="teamAndProjectScopeLabel" >Team</div>
+                                    <div class="paragraphs">Shryans Goyal, Danny Andreini</div>
+                                </div>
+                                <div class="paragraphs">
+                                    <div class="teamAndProjectScopeLabel" id="projectScope">Project Scope</div>
+                                    <div class="paragraphs">Design, Machine Learning, User Research, Cloud Compiling, Javascript, Beta Testing</div>
+
+                                </div>
+                            </div>
+                            <div>
+                                <div class="caption">THE PROBLEM</div>
+                                <div class="captionDescription">Insert description of project here. Lorem ipsum dolor sit amet, erat postea argumentum id mei, ius quaeque epicurei ut. Doming aperiri fabulas pri id, id dolor percipitur nec, sale error ei per. Dolores insolens mea ex, eam definitiones reprehendunt ei, sed eu commune omnesque sadipscing. Diam admodum offendit usu at, ei aperiam interesset quo.</div>
+                            </div>
+                            <div>
+                                <div class="caption">OUR SOLUTION</div>
+                                <div class="captionDescription">Insert description of project here. Lorem ipsum dolor sit amet, erat postea argumentum id mei, ius quaeque epicurei ut. Doming aperiri fabulas pri id, id dolor percipitur nec, sale error ei per. Dolores insolens mea ex, eam definitiones reprehendunt ei, sed eu commune omnesque sadipscing. Diam admodum offendit usu at, ei aperiam interesset quo.</div>
+                            </div>
+                            
+                        </div>              
+                    </div>
+                </div>
+            </Router>
+        )
+
     }
 
-  }
+}
