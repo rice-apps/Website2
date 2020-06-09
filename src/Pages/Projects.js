c3a55c57ad909af395e64b23637689e53fbc9c08
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-06-09 15:10:00 -0500

    Updated projects to be more extensible in the future

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,90 +1,86 @@
-import React, { Component } from 'react';
-import Fade from 'react-reveal/Fade';
-import './Projects.css';
-import {BrowserRouter as Router, Link, NavLink, Redirect} from "react-router-dom";
+import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
+import "./Projects.css";
+import {
+	BrowserRouter as Router,
+	Link,
+	NavLink,
+	Redirect,
+} from "react-router-dom";
 
-import box from '../images/box.svg'
+import box from "../images/box.svg";
 
-export default class Projects extends Component {
+const pastProjects = [
+	{
+		name: "beakspeak",
+		projectText: "Rice's anonymous, exclusive social media platform",
+		titleID: "BsTitle",
+	},
+	{
+		name: "carpool",
+		projectText: "Ride-sharing at Rice made easier",
+		titleID: "cTitle",
+	},
+	{
+		name: "website",
+		projectText: "Brand creation for organizations",
+		titleID: "wTitle",
+	},
+];
 
-  render() {
-    return(
+const futureProjects = [
+	{
+		name: "hedwig",
+		projectText: "A revolutionary pickup ordering system for Rice's retail establishments.",
+		titleID: "HwTitle",
+	},
+	{
+		name: "pdi",
+		projectText: "A new texting experience for low-income patients",
+		titleID: "pdiTitle"
+	},
+	{
+		name: "ccd",
+		projectText: "An externship matching platform for the CCD",
+		titleID: "RTitle",
+	},
+];
 
-    <div className = "projectsPageWrapper">
-     <Fade left duration = {1500}>
-        <div id = "beakspeak">
-          <div id = "BsTitle">
-            <div>
-                <NavLink to = "/beakspeak"  class = "link">
-                <h1>Beakspeak</h1>
-              </NavLink>
-            </div>
-          </div>
-          <div className = "circle">
-            <img className = "airbnb" src = {box} alt=""></img>
-            <div className = "projectText">
-            Rice's anonymous, exclusive social media platform</div>
-          </div>
-        </div>
-      </Fade>
+const ProjectBubble = ({ project }) => (
+	<div id={project.name}>
+		<div id={project.titleID}>
+			<div>
+				<NavLink to={`/${project.name}`} class="link">
+					<h1>{project.name.toLocaleUpperCase()}</h1>
+				</NavLink>
+			</div>
+		</div>
+		<div className="circle">
+			<img className="airbnb" src={box} alt="" />
+			<div className="projectText">
+				{project.projectText}
+			</div>
+		</div>
+	</div>
+)
 
-      <Fade right duration = {1500}>
-        <div id = "carpool">
-          <div id = "cTitle">
-            <NavLink to = "/carpool"  class = "link">
-              <h1>Carpool</h1>
-            </NavLink>
-          </div>
-          <div className = "circle">
-            <img className = "airbnb" src = {box} alt=""></img>
-            <div className = "projectText">Ride-sharing at Rice made easier</div>
-          </div>
-        </div>
-      </Fade>
+const Projects = ({}) => {
+	return (
+		<div className="projectsPageWrapper">
+			<div className="projectTitles">
+				<h1 className="ppTitle">Past Projects</h1>
+				<h1 className="fpTitle">Future Projects</h1>
+			</div>
+			<Fade left duration={1500}>
+				{pastProjects.map((project) => (
+					<ProjectBubble project={project} />
+				))}
+				{futureProjects.map((project) => (
+					<ProjectBubble project={project} />
+				))}
+			</Fade>
+		</div>
+	);
+};
 
-      <div id = "hedwig">
-        <div id = "HwTitle">
-          <NavLink to = "/hedwig" class = "link">
-            <h1>Hedwig</h1>
-          </NavLink>
-        </div>
-        <div className = "circle">
-          <img className = "airbnb" src = {box} alt=""></img>
-          <div className = "projectText">The Hoot's delivery app</div>
-        </div>
-      </div>
-
-      <Fade left duration = {1500}>
-        <div id = "website">
-          <div id = "wTitle">
-            <NavLink to = "/website" class = "link">
-              <h1>Website</h1>
-            </NavLink>
-          </div>
-          <div className = "circle">
-            <img className = "airbnb" src = {box} alt=""></img>
-            <div className = "projectText">Brand creation for organizations</div>
-          </div>
-        </div>
-      </Fade>
-
-      <Fade right duration = {1500}>
-        <div id = "reservations">
-          <div id = "RTitle">
-            <NavLink to = "/reservations"  class = "link">
-              <h1>Reservations</h1>
-            </NavLink>
-          </div>
-          <div className = "circle">
-            <img className = "airbnb" src = {box} alt=""></img>
-            <div className = "projectText">Reserve rooms on the Rice campus</div>
-          </div>
-        </div>
-      </Fade>
-
-    </div>
-  )
-
-  }
-
-}
+export default Projects;
