9a1872938926dbbfa5653fdd55cafe5c65478e0a
zawie <zawie@rice.edu>
2022-10-15 15:40:19 -0500

    add past partners

--- src/Pages/Partners.js
+++ src/Pages/Partners.js
@@ -6,6 +6,12 @@ import CsClub from '../images/csclub.png';
 import Coffeehouse from '../images/chaus.jpg';
 import EastWest from '../images/eastwest.png'
 import Hoot from '../images/hoot.jpeg'
+import HFB from '../images/hfb.png'
+import SS from '../images/second-servings.jpeg'
+import HFH from '../images/hives-for-heroes-partner-logo.jpeg'
+import MOODY from '../images/moody.png'
+import PDI from '../images/pdi.jpeg'
+import CCD from '../images/ccd.png'
 
 import Fade from 'react-reveal/Fade';
 export default class Partners extends Component {
@@ -18,6 +24,12 @@ export default class Partners extends Component {
                     <div id="text"> We've worked with...</div>
                     <div id="logosWrapper">
                         <div id="logos">
+                            <img id="image" src={MOODY} alt="" />
+                            <img id="image" src={SS} alt="" />
+                            <img id="image" src={HFB} alt="" />
+                            <img id="image" src={HFH} alt="" />
+                            <img id="image" src={CCD} alt="" />
+                            <img id="image" src={PDI} alt="" />
                             <img id="image" src={FOTW} alt="" />
                             <img id="image" src={BakerRipley} alt="" />
                             <img id="image" src={CsClub} alt="" />
